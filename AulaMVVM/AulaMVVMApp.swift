2086744f33f7806f4f83843ad0d5da051360954b
victorletichevsky <victorletichevsky@gmail.com>
2022-10-12 11:06:41 -0300

    Initial Commit

---
+++ AulaMVVM/AulaMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  AulaMVVMApp.swift
+//  AulaMVVM
+//
+//  Created by Victor Letichevsky on 12/10/22.
+//
+
+import SwiftUI
+
+@main
+struct AulaMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
